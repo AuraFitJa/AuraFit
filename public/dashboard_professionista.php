773f65afe3b2f130391861c5cb02938cc593141f
AuraFitJa <aurafit.team@gmail.com>
2026-02-13 13:34:47 +0100

    Change from userid to idUtente

--- public/dashboard_professionista.php
+++ public/dashboard_professionista.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['user_id'])) { header("Location: login.php"); exit; }
+if (!isset($_SESSION['idUtente'])) { header("Location: login.php"); exit; }
 echo "<h1>Dashboard Professionista</h1>";
 echo "<p>Utente: " . htmlspecialchars($_SESSION['email']) . "</p>";
 echo "<p>Ruoli: " . htmlspecialchars(implode(", ", $_SESSION['roles'] ?? [])) . "</p>";
