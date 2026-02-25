ac70f46c1d8e1442aeff31395574e76fc2e085b6
AuraFit <aurafit.team@gmail.com>
2026-02-25 21:46:41 +0100

    Update supporto.php

--- public/dashboard_clienti/supporto.php
+++ public/dashboard_clienti/supporto.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__ . '/common.php';
-require_once __DIR__ . '/../../app/Models/SupportoModel.php';
+require_once __DIR__ . '../app/Models/SupportoModel.php';
 
 $errors = [];
 $messages = [];
